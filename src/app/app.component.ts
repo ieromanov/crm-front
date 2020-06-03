6e1a5c6e46a0519f4d9c40fe03bb6544f88c0678
IERomanov <i.e.romanov1997@gmail.com>
2020-06-03 23:58:45 +0400

    fix main menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  title: string; 
+  link?: string;
+  icon?: string;
+  children?: MenuItem[]
+}
+
 @Component({
   selector: 'crm-root',
   templateUrl: './app.component.html',
@@ -7,6 +14,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public isCollapsed: boolean = false;
+
+  public menuItems: MenuItem[] = [
+    {
+      title: 'Dashboard',
+      icon: 'dashboard',
+      children: [{
+        link: '/dashboard',
+        title: 'Dashboard'
+      }]
+    },
+    {
+      title: 'Setting',
+      icon: 'setting',
+      children: [
+        {
+          link: '/setting/general',
+          title: 'General'
+        },
+        {
+          link: '/setting/status',
+          title: 'Status'
+        },
+        {
+          link: '/setting/room',
+          title: 'Room'
+        },
+        {
+          link: '/setting/home',
+          title: 'Home'
+        },
+        {
+          link: '/setting/service-type',
+          title: 'Service type'
+        },
+      ]
+    }
+  ]
 
   constructor() {}
 
