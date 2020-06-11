19f3e708cca1132b289eb11cef875f6fdf895ee4
IERomanov <i.e.romanov1997@gmail.com>
2020-06-12 01:35:06 +0400

    remove: http request interceptor
    extract logic in auth interceptor

--- src/app/core/interceptors/http-request.interceptor.ts
+++
@@ -1,9 +0,0 @@
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
-export class HttpRequestInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    req = req.clone({ withCredentials: true });
-    return next.handle(req);
-  }
-}
