7ec8d13032faa3880d1d8a02a4c94d1487519df2
IERomanov <i.e.romanov1997@gmail.com>
2020-06-12 01:34:16 +0400

    feature: use store in auth interceptor

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -8,23 +8,29 @@ import {
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import { State } from '@core/store';
+import { logoutAction } from '@core/store/user/user.action';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthInterceptor implements HttpInterceptor {
+  constructor(private readonly _store: Store<State>) {}
+
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    request = request.clone({ withCredentials: true });;
     return next
       .handle(request)
-      .pipe(catchError(this.handleAuthError));
+      .pipe(catchError(this.handleAuthError.bind(this)));
   }
 
   private handleAuthError(err: HttpErrorResponse): Observable<any> {
     if (err.status === 401) {
-      window.open('http://localhost:3000/api/auth/login', '_self')
+      this._store.dispatch(logoutAction());
     }
     return throwError(err);
   }
