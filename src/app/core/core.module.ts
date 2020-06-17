6f2c26df589c9128fa7dbce13dcb1090a46380d4
IERomanov <i.e.romanov1997@gmail.com>
2020-06-17 23:47:47 +0400

    remove: extra services into core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Router } from '@angular/router';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-
-import { NzNotificationModule } from 'ng-zorro-antd/notification';
 
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { ApiServiceProvider } from './provider/api-service.provider';
 import { AuthService } from './services/auth.service';
 
 @NgModule({
-  imports: [CommonModule, HttpClientModule, NzNotificationModule],
+  imports: [CommonModule, HttpClientModule],
   providers: [
     ApiServiceProvider,
     AuthService,
