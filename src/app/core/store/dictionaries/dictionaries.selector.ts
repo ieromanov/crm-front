3f146112e94d5635f50855b9affcce8bdef6aba0
IERomanov <i.e.romanov1997@gmail.com>
2020-06-19 00:02:39 +0400

    add: selectors for dictionaries

--- src/app/core/store/dictionaries/dictionaries.selector.ts
+++ src/app/core/store/dictionaries/dictionaries.selector.ts
@@ -1,10 +1,27 @@
 import { createSelector, createFeatureSelector } from '@ngrx/store'
 import { State } from '@store/index'
 import { IDictionariesState } from './dictionaries.reducer';
+import { IStatusDictionaryState } from './status-dictionary/status-dictionary.reducer';
+import { IHomeTypeDictionaryState } from './home-type-dictionary/home-type-dictionary.reducer';
 
 export const selectDictionariesState =  createFeatureSelector<State, IDictionariesState>('dictionaries');
 
 export const statusDictionarySelector = createSelector(
   selectDictionariesState,
-  (state: IDictionariesState) => state.statuses
+  (state: IDictionariesState) => state.statuses,
+)
+
+export const statusDictionaryEntitiesSelector = createSelector(
+  statusDictionarySelector,
+  (statuses: IStatusDictionaryState) => Object.values(statuses.entities)
+)
+
+export const homeTypesDictionarySelector = createSelector(
+  selectDictionariesState,
+  (state: IDictionariesState) => state.homeTypes,
+)
+
+export const homeTypesDictionaryEntitiesSelector = createSelector(
+  homeTypesDictionarySelector,
+  (homeTypes: IHomeTypeDictionaryState) => Object.values(homeTypes.entities)
 )
