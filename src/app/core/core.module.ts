0f23cb584e0a1a0b6c4165befb7b3cfdb8f23f01
ieromanov <i.e.romanov1997@gmail.com>
2020-07-25 00:58:32 +0400

    add: provided http error interceptor in core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { HttpErrorInterceptor } from './interceptors/http-error.interceptor';
+
 import { AuthService } from './services/auth.service';
 import { ApiService } from './services/api.service';
 
@@ -14,8 +16,13 @@ import { ApiService } from './services/api.service';
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
-      multi: true
-    }
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorInterceptor,
+      multi: true,
+    },
   ],
 })
 export class CoreModule {}
